a637ef29213146f22fc4c66f6bd6ec917c086d91
Aiglon2285 <87932694+Aiglon2285@users.noreply.github.com>
2021-07-25 14:13:14 +0200

    Update fun.js

--- fun.js
+++ fun.js
@@ -99,46 +99,7 @@ bot.on("message", message => {
   })
     
   
-  
-  
-  
-  
-  
-  /********** MEME GENERATOR ************/
-  
-  
-  
-  
-  bot.on('message', message => {
-      if(message.content.startsWith("!meme")) {
-  
-  const embed = new Discord.MessageEmbed();
-      got('https://www.reddit.com/r/memes/random/.json') 
-          .then(response => {
-              const [list] = JSON.parse(response.body);
-              const [post] = list.data.children;
-  
-              const permalink = post.data.permalink;
-              const memeUrl = `https://reddit.com${permalink}`;
-              const memeImage = post.data.url;
-              const memeTitle = post.data.title;
-              const memeUpvotes = post.data.ups;
-              const memeNumComments = post.data.num_comments;
-  
-              embed.setTitle(`${memeTitle}`);
-              embed.setURL(`${memeUrl}`);
-              embed.setColor('RANDOM');
-              embed.setImage(memeImage);
-              embed.setFooter(`👍 ${memeUpvotes} 💬 ${memeNumComments}`);
-  
-              message.channel.send(embed);
-          })
-          .catch(console.error);
-  }});
-  
-  
-  
-  
+
   
   
   
